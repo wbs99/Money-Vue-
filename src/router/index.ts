89a6ab309e03bdde7fc35822a883d7c9c3df827a
wbs <1134954328@qq.com>
2021-07-29 09:34:58 +0800

    全局组件 Nav

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,5 @@
-
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '@/views/Home.vue';
 import Money from '@/views/Money.vue';
 import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue';
@@ -11,21 +9,20 @@ Vue.use(VueRouter);
 const routes = [
   {
     path: '/',
-
     redirect: '/money'
   },
   {
-path: '/money',
-  component: Money
-},
-{
-  path: '/labels',
+    path: '/money',
+    component: Money
+  },
+  {
+    path: '/labels',
     component: Labels
-},
-{
-  path: '/statistics',
+  },
+  {
+    path: '/statistics',
     component: Statistics
-},
+  },
 ];
 
 const router = new VueRouter({
