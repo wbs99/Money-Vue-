02ff36a57a36117934c9ea01e434d0795ed3ba96
wbs <1134954328@qq.com>
2021-07-29 10:42:21 +0800

    使用 svg-sprite-loader-mod 解决 scss import 问题

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -10,3 +10,10 @@ export default {
   name: 'Money',
 };
 </script>
+
+<style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
+p{
+  color: $red;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,16 @@
-/* eslint-disable */
 const path = require('path')
-
 module.exports = {
   lintOnSave: false,
-  chainWebpack: config => {
+  chainWebpack: config =>{
     const dir = path.resolve(__dirname, 'src/assets/icons')
-
     config.module
       .rule('svg-sprite')
       .test(/\.svg$/)
       .include.add(dir).end() // 包含 icons 目录
-      .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract: false}).end()
-    config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
+      .use('svg-sprite-loader-mod').loader('svg-sprite-loader-mod').options({extract:false}).end()
+      .use('svgo-loader').loader('svgo-loader')
+      .tap(options => ({...options, plugins: [{removeAttrs: {attrs: 'fill'}}]})).end()
+    config.plugin('svg-sprite').use(require('svg-sprite-loader-mod/plugin'), [{plainSprite: true}])
     config.module.rule('svg').exclude.add(dir) // 其他 svg loader 排除 icons 目录
 
 
@@ -25,6 +24,5 @@ module.exports = {
     //   .end()
     // config.plugin('svg-sprite').use(require('svg-sprite-loader-mod/plugin'), [{plainSprite: true}])
     // config.module.rule('svg').exclude.add(dir)
-
   }
 }
