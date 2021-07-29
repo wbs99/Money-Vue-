286935c53ee2ccb810a8c975473308a0e63e76b4
wbs <1134954328@qq.com>
2021-07-29 10:16:54 +0800

    创建 Icon.vue

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,12 +1,22 @@
 <template>
-    <div class="nav">
-      <router-link to="/money">记账</router-link>
-      |
-      <router-link to="/labels">标签</router-link>
-      |
-      <router-link to="/statistics">统计</router-link>
-    </div>
+  <div class="nav">
+    <router-link to="/money">
+      <Icon name="money"/>
+      记账
+    </router-link>
+    |
+    <router-link to="/labels">
+      <Icon name="label"/>
+      标签
+    </router-link>
+    |
+    <router-link to="/statistics">
+      <Icon name="statistics"/>
+      统计
+    </router-link>
+  </div>
 </template>
+
 <script lang="ts">
 export default {
   name: 'Nav'
@@ -14,5 +24,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.nav{border:1px solid red;}
+.nav {border: 1px solid red;}
 </style>
