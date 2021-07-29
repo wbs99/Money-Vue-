5484d73b06c64b1eb33977df65c54a208f46f39f
wbs <1134954328@qq.com>
2021-07-29 15:40:15 +0800

    改造 Notes.vue 使其成为通用组件

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -2,7 +2,10 @@
   <Layout class-prefix="layout">
     <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
     <Types :value.sync="record.type"/>
-    <Notes @update:value="onUpdateNotes"/>
+    <Notes field-name="备注"
+           placeholder="在这里输入备注"
+           @update:value="onUpdateNotes"
+    />
     <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
   </Layout>
 </template>
@@ -15,6 +18,7 @@ import Tags from '@/components/Money/Tags.vue';
 import {Component, Watch} from 'vue-property-decorator';
 import recordListModel from '@/models/recordListModel';
 import tagListModel from '@/models/tagListModel';
+
 const recordList = recordListModel.fetch();
 const tagList = tagListModel.fetch();
 @Component({
@@ -26,17 +30,21 @@ export default class Money extends Vue {
   record: RecordItem = {
     tags: [], notes: '', type: '-', amount: 0
   };
+
   onUpdateTags(value: string[]) {
     this.record.tags = value;
   }
+
   onUpdateNotes(value: string) {
     this.record.notes = value;
   }
+
   saveRecord() {
     const record2: RecordItem = recordListModel.clone(this.record);
     record2.createdAt = new Date();
     this.recordList.push(record2);
   }
+
   @Watch('recordList')
   onRecordListChange() {
     recordListModel.save(this.recordList);
