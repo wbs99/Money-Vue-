b97a4cb7ef8274d9ef56fc8e26fa8f63557af780
wbs <1134954328@qq.com>
2021-07-30 10:50:36 +0800

    封装 window.createTag

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -3,12 +3,11 @@ import clone from '@/lib/clone';
 const localStorageKeyName = 'recordList';
   const recordListModel = {
     data: [] as RecordItem[],
-
     fetch() {
       this.data =  JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
       return this.data
     },
-    create(record :RecordItem[]){
+    create(record :RecordItem){
       const record2: RecordItem = clone(record);
       record2.createdAt = new Date();
       this.data.push(record2);
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -22,25 +22,28 @@ import recordListModel from '@/models/recordListModel';
 import tagListModel from '@/models/tagListModel';
 
 const recordList = recordListModel.fetch();
-const tagList = tagListModel.fetch();
 @Component({
   components: {Tags, FormItem, Types, NumberPad}
 })
 export default class Money extends Vue {
-  tags = tagList;
+  tags = window.tagList
   recordList: RecordItem[] = recordList;
   record: RecordItem = {
     tags: [], notes: '', type: '-', amount: 0
   };
+
   onUpdateTags(value: string[]) {
     this.record.tags = value;
   }
+
   onUpdateNotes(value: string) {
     this.record.notes = value;
   }
+
   saveRecord() {
-    recordListModel.create()
+    recordListModel.create(this.record);
   }
+
   @Watch('recordList')
   onRecordListChange() {
     recordListModel.save();
@@ -52,7 +55,8 @@ export default class Money extends Vue {
   display: flex;
   flex-direction: column-reverse;
 }
-  .notes {
-    padding: 12px 0;
-  }
+
+.notes {
+  padding: 12px 0;
+}
 </style>
