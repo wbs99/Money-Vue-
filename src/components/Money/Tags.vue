6ba3ab0883a2a989a3f14501cf72701ef0fa38c4
wbs <1134954328@qq.com>
2021-07-31 14:52:40 +0800

    Tags.vue 使用 Vuex

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -15,17 +15,21 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import {Component, Prop} from 'vue-property-decorator';
+import {Component} from 'vue-property-decorator';
 
 @Component({
   computed: {
     tagList() {
-      return this.$store.fetchTags()
+      return this.$store.state.tagList;
     }
   }
 })
 export default class Tags extends Vue {
   selectedTags: string[] = [];
+
+  created() {
+    this.$store.commit('fetchTags');
+  }
 
   toggle(tag: string) {
     const index = this.selectedTags.indexOf(tag);
@@ -39,9 +43,8 @@ export default class Tags extends Vue {
 
   create() {
     const name = window.prompt('请输入标签名');
-
     if (!name) { return window.alert('标签名不能为空'); }
-    store.createTag(name);
+    this.$store.commit('createTag', name);
   }
 }
 </script>
