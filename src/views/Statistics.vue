01f6098a1e7a1f816944e62d8e62c4853be6cce0
wbs <1134954328@qq.com>
2021-09-09 17:04:23 +0800

    使用计数排序构造 30 天的数据

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -31,6 +31,8 @@ import recordTypeList from '@/constants/recordTypeList';
 import dayjs from 'dayjs';
 import clone from '@/lib/clone';
 import Chart from '@/components/Money/Chart.vue';
+import _ from 'lodash';
+import day from 'dayjs';
 
 @Component({
   components: {Tabs, Chart},
@@ -41,7 +43,8 @@ export default class Statistics extends Vue {
   }
 
   mounted() {
-    (this.$refs.chartWrapper as HTMLDivElement).scrollLeft = 9999;
+    const div = (this.$refs.chartWrapper as HTMLDivElement);
+    div.scrollLeft = div.scrollWidth;
   }
 
   beautify(string: string) {
@@ -60,7 +63,33 @@ export default class Statistics extends Vue {
     }
   }
 
+  get y(){
+    const today = new Date();
+    const array = [];
+    // 获取 30 个日期，每个日期就是今天减去 i 天
+    // 再找到这一天对应的 amount
+    for (let i = 0; i <= 29; i++) {
+      const dateString = day(today).subtract(i, 'day').format('YYYY-MM-DD');
+      array.push({
+        date: dateString,
+        value: _.find(this.recordList, {createdAt: dateString})?.amount
+      });
+    }
+    array.sort((a,b)=>{
+      if(a.date>b.date){
+        return 1
+      }else if(a.date===b.date){
+        return 0
+      }else{
+        return -1
+      }
+    })
+    return array
+  }
+
   get x() {
+    const keys = this.y.map(item=>item.date)
+    const values = this.y.map(item=>item.value)
     return {
       grid: {
         left: 0,
@@ -68,11 +97,7 @@ export default class Statistics extends Vue {
       },
       xAxis: {
         type: 'category',
-        data: [
-          '1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
-          '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
-          '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
-        ],
+        data: keys,
         axisTick: {alignWithLabel: true}
       },
       yAxis: {
@@ -83,17 +108,12 @@ export default class Statistics extends Vue {
         symbol: 'circle',
         symbolSize: 13,
         itemStyle: {borderWidth: 1, color: '#eaa6a3'},
-        data: [
-          820, 932, 901, 934, 1290, 1330, 1320,
-          820, 932, 901, 934, 1290, 1330, 1320,
-          820, 932, 901, 934, 1290, 1330, 1320,
-          820, 932, 901, 934, 1290, 1330, 1320, 1, 2
-        ],
+        data: values,
         type: 'line'
       }],
       tooltip: {
         show: true,
-        position:'top',
+        position: 'top',
         formatter: '{c}'
       }
     };
